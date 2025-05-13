9231fa2757dcd49b2025ed5b01e0651be40df5b5
Zabuka_zuzu <zuzu950444@gmail.com>
2025-05-13 09:09:15 +0000

    mistify: replaced mist audio

--- config/common.mk
+++ config/common.mk
@@ -106,7 +106,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
     debug.photos.p_editr.eraser=1
 
 # Inherit from audio config
-$(call inherit-product, vendor/pixel-style/config/audio.mk)
+#$(call inherit-product, vendor/pixel-style/config/audio.mk)
 
 # Inherit from rro_overlays config
 $(call inherit-product, vendor/pixel-style/config/rro_overlays.mk)
